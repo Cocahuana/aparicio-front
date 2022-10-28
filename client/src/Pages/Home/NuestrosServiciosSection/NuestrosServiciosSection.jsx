846ec88d38fd986512fdfe10bf4ce0f623852cbf
Ezequiel <ezequieldominguez11@gmail.com>
2022-10-28 14:26:01 -0300

    Servicios component finished in Home

--- client/src/Pages/Home/NuestrosServiciosSection/NuestrosServiciosSection.jsx
+++ client/src/Pages/Home/NuestrosServiciosSection/NuestrosServiciosSection.jsx
@@ -1,84 +1,64 @@
 import CenteredSectionTitle from "../../../components/CenteredSectionTitle/CenteredSectionTitle";
 import FeatureDivider from "../../../components/FeatureDivider/FeatureDivider";
+import carga_de_gasImg from "./assets/carga_de_gas.png";
+import instalacion_de_aireImg from "./assets/instalacion_de_aire.jpg";
+import mantenimientoImg from "./assets/mantenimiento.jpg";
+import {Card, Col, Button, Row} from "react-bootstrap";
 
 function NuestrosServiciosSection() {
+	const SERVICIOS = [
+		{
+			img: instalacion_de_aireImg,
+			alt: "Servicio: instalacion de aire",
+			title: "Instalación de equipos de aire acondicionado",
+			buttonText: "Más información",
+			link: "/Páginas/Servicios/instalación.aspx",
+		},
+		{
+			img: mantenimientoImg,
+			alt: "Servicio: instalacion de aire",
+			title: "Mantenimiento de equipos de aire acondicionado",
+			buttonText: "Más información",
+			link: "/Páginas/Servicios/mantenimiento.aspx",
+		},
+		{
+			img: carga_de_gasImg,
+			alt: "Servicio: instalacion de aire",
+			title: "Carga de gas refrigerante",
+			buttonText: "Más información",
+			link: "/Páginas/Servicios/carga_refrigerante.aspx",
+		},
+	];
 	return (
 		<>
 			<CenteredSectionTitle title='ALGUNOS DE NUESTROS SERVICIOS' />
 
-			<div
-				className='row row-cols-1 row-cols-md-3 g-4 pt-5 pb-5'
-				style={{backgroundColor: "#f0f3f6"}}>
-				<div className='col'>
-					<div className='card h-100'>
-						<img
-							src='/Img/Inicio/instalacion_de_aire.jpg'
-							className='card-img-top'
-							alt='...'
-						/>
-						<div className='card-footer text-center'>
-							<p className=''>
-								<strong>
-									Instalación de equipos de aire acondicionado
-								</strong>
-							</p>
-							<p className='card-text'>
-								<a
-									className='btn btn-primary'
-									href='/Páginas/Servicios/instalación.aspx'>
-									Más información
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div className='col'>
-					<div className='card h-100'>
-						<img
-							src='/Img/Inicio/mantenimiento.jpg'
-							className='card-img-top'
-							alt='...'
-						/>
-						<div className='card-footer text-center'>
-							<p className=''>
-								<strong>
-									Mantenimiento de equipos de aire
-									acondicionado
-								</strong>
-							</p>
-							<p className='card-text'>
-								<a
-									className='btn btn-primary'
-									href='/Páginas/Servicios/mantenimiento.aspx'>
-									Más información
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div className='col'>
-					<div className='card h-100'>
-						<img
-							src='/Img/Inicio/carga_de_gas.png'
-							className='card-img-top'
-							alt='...'
-						/>
-						<div className='card-footer text-center'>
-							<br />
-							<p className=''>
-								<strong>Carga de gas refrigerante</strong>
-							</p>
-							<p className='card-text'>
-								<a
-									className='btn btn-primary'
-									href='/Páginas/Servicios/carga_refrigerante.aspx'>
-									Más información
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Row className='pt-5 pb-5' style={{backgroundColor: "#f0f3f6"}}>
+				{SERVICIOS.map((servicio) => (
+					<Col>
+						<Card className='h-100'>
+							<Card.Img
+								variant='top'
+								src={servicio.img}
+								alt={servicio.alt}
+							/>
+							<Card.Body
+								className='text-center'
+								style={{
+									display: "flex",
+									flexGrow: "1",
+									flexDirection: "column",
+									justifyContent: "space-between",
+								}}>
+								<Card.Title>{servicio.title}</Card.Title>
+								<Button variant='primary'>
+									{servicio.buttonText}
+								</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+				))}
+			</Row>
 		</>
 	);
 }
